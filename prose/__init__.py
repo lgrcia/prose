3cf0d153177172d97fa6f0ecb08d014c55e33a3f
Lionel Garcia <lionelgarcia@MacBook-Pro-de-Lionel.local>
2020-04-29 01:01:24 +0200

    add access to prose objects in __main__

--- prose/__init__.py
+++ prose/__init__.py
@@ -1,7 +1,14 @@
 __version__ = "0.0.1"
 
+import warnings
+from astropy.wcs import FITSFixedWarning
+warnings.simplefilter("ignore", FITSFixedWarning)
+
 from prose import config
 
 CONFIG = config.ConfigManager()
 
 from .products.photometry import Photometry
+from prose.lightcurves import LightCurve, LightCurves
+from prose.io import FitsManager
+from prose.telescope import Telescope
