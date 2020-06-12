55dd3c25a99c16597ef293d7b33a68ee86689027
Lionel Garcia <lionelgarcia@nebulium.astro.ulg.ac.be>
2020-06-12 18:09:14 +0200

    updating test to new features

--- test/test.py
+++ test/test.py
@@ -1,22 +1,30 @@
-from prose import pipeline
-from prose import Photometry
+from prose import pipeline, Photometry, Telescope, FitsManager
 import matplotlib.pyplot as plt
 import os
 import shutil
 from os import path
 import unittest
 
-RAW = "test_data/minimal_quatar2b_dataset"
-_REDUCED = "test_data/_minimal_quatar2b_dataset_reduced"
-REDUCED = "test_data/minimal_quatar2b_dataset_reduced"
+RAW = "test/minimal_quatar2b_dataset"
+_REDUCED = "test/_minimal_quatar2b_dataset_reduced"
+REDUCED = "test/minimal_quatar2b_dataset_reduced"
 
 class TestReduction(unittest.TestCase):
 
     def test_reduction(self):
-        reduction = pipeline.Reduction(RAW, deepness=2)
+
+        _ = Telescope({
+            "name": "test",
+            "trimming": [40, 40],
+            "pixel_scale": 0.66,
+            "latlong": [31.2027, 7.8586]
+        })
+
+        fm = FitsManager(RAW, depth=2, )
+        reduction = pipeline.Reduction(fits_manager=fm)
         reduction.set_observation(0, check_calib_telescope=False)
 
-        reduction.run(_REDUCED, save_gif=True, overwrite=True)
+        reduction.run(_REDUCED, overwrite=True)
 
         photometry = pipeline.Photometry(_REDUCED)
         photometry.run(overwrite=True)
