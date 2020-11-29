9d51a176ebf7967db45aa925111c2a3f2708032a
Lionel Garcia <lionelgarcia@nebulium.astro.ulg.ac.be>
2020-11-29 16:46:27 +0100

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="prose",
-    version="0.8.2",
+    version="0.8.3",
     author="Lionel J. Garcia",
     description="Reduction and analysis of FITS telescope observations",
     py_modules=["prose"],
