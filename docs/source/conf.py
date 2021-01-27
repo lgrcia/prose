0fe4e5c0bcfd7be0d1ec6c335efa9c3f05494341
Lionel <lionel_garcia@live.fr>
2021-01-27 15:26:29 +0100

    fix conf.py cwd added

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,9 +9,7 @@
 import os
 import sys
 
-sys.path.insert(1, os.path.dirname(os.path.abspath(__file__)))
-
-# sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
 
