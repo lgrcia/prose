1441cff26940850742bfe922ad2c7381ae591cb7
mathtimm <mathilde.timmermans@hotmail.com>
2021-04-11 12:49:36 +0200

    Add TTF link to builtins.

--- prose/builtins.py
+++ prose/builtins.py
@@ -24,7 +24,13 @@ speculoos_south = {
     "altitude": 2000,  # in meters
     "diameter": 100,  # in meters
     "pixel_scale": 0.33,  # in arcseconds
-    "latlong": [-24.6275, -70.4044]  # [latitude, longitude] in degree
+    "latlong": [-24.6275, -70.4044], # [latitude, longitude] in degree
+    "TTF_link": "https://astro.swarthmore.edu/telescope/tess-secure/print_eclipses.cgi?observatory_string=-24.6272%3B-70.4039%3"
+                "BAmerica%2FSantiago%3BSPECULOOS-South+Observatory+%284x1.0-m%29%3BSPECULOOS+South&use_utc=1&observatory_latitude=-29.2563"
+                "&observatory_longitude=-70.738&timezone=UTC&start_date={date}&days_to_print=90&days_in_past=0&minimum_start_elevation="
+                "23&and_vs_or=or&minimum_end_elevation=23&minimum_ha=-12&maximum_ha=12&baseline_hrs=1.5&show_unc=1&maximum_priority=4&"
+                "minimum_depth=0&maximum_V_mag=&target_string={tic_id}&lco_only=0&single_object=0&ra=&dec=&epoch=&period=&duration=&"
+                "target=&show_ephemeris=0&print_html=1&twilight=-12&max_airmass=2.4"
 }
 
 callisto = speculoos_south.copy()
@@ -43,7 +49,12 @@ artemis = speculoos_south.copy()
 artemis.update({
     "name": "Artemis",
     "ra_unit": "hourangle",
-    "latlong": [28.4754, -16.3089]})
+    "latlong": [28.4754, -16.3089],
+    "TTF_link": "https://astro.swarthmore.edu/telescope/tess-secure/print_eclipses.cgi?observatory_string=28.2999%3B-16.5058%3BAtlantic%2FCanary%3"
+                "BSPECULOOS-Northern+Observatory+1.0m%3BSPECULOOS-North+1.0m&use_utc=1&observatory_latitude=-29.2563&observatory_longitude=-70.738&"
+                "timezone=UTC&start_date={date}&days_to_print=90&days_in_past=0&minimum_start_elevation=23&and_vs_or=or&minimum_end_elevation=23&"
+                "minimum_ha=-12&maximum_ha=12&baseline_hrs=1.5&show_unc=1&maximum_priority=4&minimum_depth=0&maximum_V_mag=&target_string={tic_id}"
+                "&lco_only=0&single_object=0&ra=&dec=&epoch=&period=&duration=&target=&show_ephemeris=0&print_html=1&twilight=-12&max_airmass=2.4"})
 
 trappist = speculoos_south.copy()
 trappist.update({
@@ -51,20 +62,36 @@ trappist.update({
     "trimming": [40, 40],
     "pixel_scale": 0.64,
     "ra_unit": "deg",
-    "latlong": [-29.2563, -70.7380]})
+    "latlong": [-29.2563, -70.7380],
+    "TTF_link": "https://astro.swarthmore.edu/telescope/tess-secure/print_eclipses.cgi?observatory_string=-29.2563%3B-70.738%3BAmerica%2FSantiago%3" \
+                "BTRAPPIST-South+%280.6-m%29%3BTRAPPIST+South&use_utc=1&observatory_latitude=-29.2563&observatory_longitude=-70.738&timezone=UTC&" \
+                "start_date={date}&days_to_print=90&days_in_past=0&minimum_start_elevation=23&and_vs_or=or&minimum_end_elevation=23&minimum_ha=-12" \
+                "&maximum_ha=12&baseline_hrs=1.5&show_unc=1&maximum_priority=4&minimum_depth=0&maximum_V_mag=&target_string={tic_id}&lco_only=0&" \
+                "single_object=0&ra=&dec=&epoch=&period=&duration=&target=&show_ephemeris=0&print_html=1&twilight=-12&max_airmass=2.4"})
 
 trappistN = trappist.copy()
 trappistN.update({
     "name": "Trappist-North",  
     "pixel_scale": 0.60,
     "ra_unit": "hourangle",
-    "latlong": [31.2027, -7.8586]})
+    "latlong": [31.2027, -7.8586],
+    "TTF_link": "https://astro.swarthmore.edu/telescope/tess-secure/print_eclipses.cgi?observatory_string=31.2061%3B-7.8664%3BAfrica%2FCasablanca%3"
+                "BTRAPPIST-North+%280.6-m%29%3BTRAPPIST+North&use_utc=1&observatory_latitude=-29.2563&observatory_longitude=-70.738&timezone=UTC&"
+                "start_date={date}&days_to_print=90&days_in_past=0&minimum_start_elevation=23&and_vs_or=or&minimum_end_elevation=23&minimum_ha=-12"
+                "&maximum_ha=12&baseline_hrs=1.5&show_unc=1&maximum_priority=4&minimum_depth=0&maximum_V_mag=&target_string={tic_id}&lco_only=0&"
+                "single_object=0&ra=&dec=&epoch=&period=&duration=&target=&show_ephemeris=0&print_html=1&twilight=-12&max_airmass=2.4"})
 
 saintex = artemis.copy()
 saintex.update({
     "name": "Saint-Ex",  
     "ra_unit": "hourangle",
-    "latlong": [31.0439, -115.4637]})
+    "latlong": [31.0439, -115.4637],
+    "TTF_link": "https://astro.swarthmore.edu/telescope/tess-secure/print_eclipses.cgi?observatory_string=31.04342%3B-115.45476%3BPST8PDT%3"
+                "BSAINT-EX+1.0m%3BSAINT-EX+1.0m&use_utc=1&observatory_latitude=-29.2563&observatory_longitude=-70.738&timezone=UTC&"
+                "start_date={date}&days_to_print=90&days_in_past=0&minimum_start_elevation=23&and_vs_or=or&minimum_end_elevation=23&"
+                "minimum_ha=-12&maximum_ha=12&baseline_hrs=1.5&show_unc=1&maximum_priority=4&minimum_depth=0&maximum_V_mag=&"
+                "target_string={tic_id}&lco_only=0&single_object=0&ra=&dec=&epoch=&period=&duration=&target=&show_ephemeris=0&"
+                "print_html=1&twilight=-12&max_airmass=2.4"})
 
 liverpool = speculoos_south.copy()
 liverpool.update ({
@@ -85,7 +112,8 @@ liverpool.update ({
     "keyword_observatory": "TELESCOP",
     "keyword_jd": "JD_UTC",
     "keyword_ra": "RA",
-    "keyword_dec": "DEC"
+    "keyword_dec": "DEC",
+    "TTF_link": None
 })
 
 built_in_telescopes = {
