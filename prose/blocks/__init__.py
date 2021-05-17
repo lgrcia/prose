90e7fa8d44df9c2cec3af1eb49552af39c84ac04
Lionel J. Garcia <lgarcia@uliege.be>
2021-05-17 17:17:02 +0200

    add FWHM abc method

--- prose/blocks/__init__.py
+++ prose/blocks/__init__.py
@@ -2,7 +2,7 @@ from .registration import XYShift, AstroAlignShift, _Twirl, Twirl
 from .alignment import AffineTransform, Cutout2D
 from .detection import SegmentedPeaks, DAOFindStars, SEDetection, Peaks
 from .calibration import Calibration, Trim
-from .psf import Gaussian2D, Moffat2D, FastGaussian, KeepGoodStars
+from .psf import Gaussian2D, Moffat2D, FastGaussian, KeepGoodStars, FWHM
 from .centroids import BalletCentroid
 from .photometry import PhotutilsAperturePhotometry, SEAperturePhotometry, PhotutilsPSFPhotometry
 from .imutils import *
