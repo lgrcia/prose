a1ab7d859d9703d6aa0c850cf850bfe4d031cf66
Lionel <lionel_garcia@live.fr>
2021-05-19 15:10:51 +0200

    version++

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="prose",
-    version="0.9.2",
+    version="0.9.3",
     author="Lionel J. Garcia",
     description="Reduction and analysis of FITS telescope observations",
     packages=["prose"],
