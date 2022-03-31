891cffc30634ffe6114523c3547c53f266f848a7
mathildetimmermans <mathilde.timmermans@hotmail.com>
2022-03-31 11:18:31 +0200

    Add TRAPPIST to builtins

--- prose/builtins.py
+++ prose/builtins.py
@@ -100,7 +100,7 @@ artemis = dict(
 trappist = dict(
     speculoos_south,
     name = "TRAPPIST-South",
-    names = ["TRAPPIST-S"],
+    names = ["TRAPPIST-S","TRAPPIST"],
     trimming= [40, 40],
     pixel_scale= 0.64,
     ra_unit= "deg",
