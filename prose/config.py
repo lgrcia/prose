26cddf59b6c032faebdbd0e26e19f849a72b454b
lgrcia <lionel_garcia@live.fr>
2022-07-08 17:32:09 +0200

    some custom logging in files + reformat colors

--- prose/config.py
+++ prose/config.py
@@ -11,59 +11,6 @@ import requests
 info = print
 package_name = "prose"
 
-
-# duplicate in io.py
-def get_files(
-    ext,
-    folder,
-    depth=0,
-    return_folders=False,
-    single_list_removal=True,
-    none_for_empty=False,
-):
-    """
-
-    Return files of specific extension in the specified folder and sub-folders
-
-    Parameters
-    ----------
-    folder : str
-        Folder to be analyzed
-    depth : int
-        Number how sub-folder layer to look into.
-        0 (default) will look into current folder
-        1 will look into current folder and its sub-folders
-        2 will look into current folder, its sub-folders and their sub-folders
-        ... etc
-
-    Returns
-    -------
-    list of fits files
-
-    """
-    files = []
-    for depth in range(depth + 1):
-        files += glob.iglob(
-            path.join(folder, "*/" * depth + "*{}".format(ext)), recursive=False
-        )
-
-    files = [path.abspath(f) for f in files if path.isfile(f)]
-
-    if return_folders:
-        folders = [path.dirname(file) for file in files]
-        if single_list_removal and len(folders) == 1:
-            return folders[0]
-        else:
-            return folders
-    else:
-        if single_list_removal and len(files) == 1:
-            return files[0]
-        elif len(files) == 0 and none_for_empty:
-            return None
-        else:
-            return files
-
-
 class ConfigManager:
     def __init__(self):
 
@@ -79,6 +26,7 @@ class ConfigManager:
         self.check_config_file(load=True)
         self.telescopes_dict = self.build_telescopes_dict()
         self.check_ballet()
+        self.logs = []
 
     def check_config_file(self, load=False):
 
