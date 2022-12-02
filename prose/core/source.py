9a254c3c51ac6cb507ff0deffa6dccdd9c005172
lgrcia <lionel_garcia@live.fr>
2022-12-02 20:51:13 +0100

    add eccentricity, set semi axis and major refactoring

--- prose/core/source.py
+++ prose/core/source.py
@@ -2,6 +2,7 @@ from matplotlib.patches import Ellipse, Circle
 from photutils.aperture import *
 from photutils.isophote import EllipseGeometry
 from photutils.isophote import Ellipse as IsoEllipse
+from astropy.utils import lazyproperty
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -23,7 +24,16 @@ def clean_stars_positions(positions, tolerance=50):
             keep.append(i)
 
     return np.unique(keep)
-    
+
+# Note: Why not using photutils.segmentation.SourceCatalog?
+# source: https://photutils.readthedocs.io/en/stable/api/photutils.segmentation.SourceCatalog.html#photutils.segmentation.SourceCatalog
+# 
+# Main reason is full control and no need to subclass SourceCatalog. Reasons:
+# - Ability to Source.plot and Source.aperture differently depending of the type of source
+# - Ability to easily instantiate a fake/incomplete source only defined by its coords (output of many detection algorithms like DAOPHOT)
+# - We will use it, as region so that users have access to it if needed
+# - I don't like this as_scalar behavior, I prefer separate Source and Sources
+
 class Source:
     def __init__(self, region=None, coords=None, peak=0, i=None):
         """Representation of a source in an image
@@ -46,9 +56,9 @@ class Source:
         #     AssertionError("Providing 'peak' has no effect when 'region' is provided")
 
         if region is not None:
-            self.a = region.axis_major_length
-            self.b = region.axis_minor_length
-            self.orientation = region.orientation
+            self.a = region.axis_major_length/2
+            self.b = region.axis_minor_length/2
+            self.orientation = np.pi/2 - region.orientation
             self.coords = np.array(region.centroid_weighted[::-1])
             self.peak = region.intensity_max
         else:
@@ -71,14 +81,9 @@ class Source:
         np.array
             vertexes coordinates
         """
-        x0, y0 = self.coords
         theta = self.orientation
-        x1 = x0 - np.sin(np.pi+theta) * 0.5 * self.a
-        y1 = y0 - np.cos(np.pi+theta) * 0.5 * self.a
-        x2 = x0 - np.sin(theta) * 0.5 * self.a
-        y2 = y0 - np.cos(theta) * 0.5 * self.a
-
-        return np.array([x1, y1]), np.array([x2, y2])
+        shifts = np.array([np.cos(theta), np.sin(theta)])*self.a
+        return self.coords + (shifts[:, None]*[-1, 1]).T
 
     @property
     def co_vertexes(self):
@@ -89,14 +94,13 @@ class Source:
         np.array
             co-vertexes coordinates
         """
-        x0, y0 = self.coords
         theta = self.orientation + np.pi/2
-        x1 = x0 - np.sin(np.pi+theta) * 0.5 * self.b
-        y1 = y0 - np.cos(np.pi+theta) * 0.5 * self.b
-        x2 = x0 - np.sin(theta) * 0.5 * self.b
-        y2 = y0 - np.cos(theta) * 0.5 * self.b
+        shifts = np.array([np.cos(theta), np.sin(theta)])*self.b
+        return self.coords + (shifts[:, None]*[-1, 1]).T
 
-        return np.array([x1, y1]), np.array([x2, y2])
+    @lazyproperty
+    def eccentricity(self):
+        return self.b/self.a
 
     def copy(self):
         """Return a copy of the Source
@@ -138,8 +142,8 @@ class Source:
         ax.add_artist(circle)
         if label and self.i is not None:
             plt.text(*(np.array(self.coords) - [0, 1.5*radius]), self.i, c=c, ha="center", va="top", fontsize=fontsize)
-        
-    def plot_ellipse(self, n=10, c=color, ax=None, label=True, fontsize=12, **kwargs):
+    
+    def plot_ellipse(self, a=None, c=color, ax=None, label=True, fontsize=12, **kwargs):
         """Plot an ellipse centered on source, with semi-major/minor length defined by the source itself 
 
         Parameters
@@ -158,12 +162,14 @@ class Source:
         if ax is None:
             ax = plt.gca()
 
+        if a is None:
+            a = 2*self.a * 1.1
         ax = plt.gca()
         e = Ellipse(
             xy=self.coords,
-            width=n + self.b,
-            height=n + self.a,
-            angle=-self.orientation * 180. / np.pi,
+            width=a,
+            height=a*self.eccentricity,
+            angle=np.rad2deg(self.orientation),
             **kwargs
         )
         e.set_facecolor('none')
@@ -171,8 +177,8 @@ class Source:
         ax.add_artist(e) 
     
         if label and self.i is not None:
-            rad = np.pi/2 + self.orientation
-            label_coord = self.coords + [0, -(np.abs(self.a*rad/2) + self.b/2)]
+            rad = self.orientation
+            label_coord = self.coords + [0, -(np.abs(self.a*rad) + self.b)]
             plt.text(*label_coord, self.i, c=c, ha="center", va="top", fontsize=fontsize)
 
     def circular_aperture(self, r, scale=True):
@@ -210,44 +216,44 @@ class Source:
         photutils.aperture.CircularAperture
         """
         if scale:
-            _a, _b = r*self.a, r*self.b
+            a, b = r*self.a, r*self.b
         else:
-            _a, _b = r, r*self.b/self.a
-        return EllipticalAperture(self.coords, _a, _b, np.pi/2-self.orientation)
+            a, b = r, r*self.eccentricity
+        return EllipticalAperture(self.coords, a, b, self.orientation)
 
     def rectangular_aperture(self, r, scale=True):
         if scale:
-            _a, _b = r*self.a, r*self.b
+            a, b = 2*r*self.a, 2*r*self.b
         else:
-            _a, _b = r, r*self.b/self.a
-        return RectangularAperture(self.coords, _a, _b, np.pi/2-self.orientation)
+            a, b = 2*r, 2*r*self.eccentricity
+        return RectangularAperture(self.coords, a, b, self.orientation)
 
     def circular_annulus(self, r0, r1, scale=False):
         if scale:
-            _r0 = r0*self.a
-            _r1 = r1*self.a
+            r0 = r0*self.a
+            r1 = r1*self.a
         else:
-            _r0 = r0
-            _r1 = r1
-        return CircularAnnulus(self.coords, _r0, _r1)
+            r0 = r0
+            r1 = r1
+        return CircularAnnulus(self.coords, r0, r1)
 
     def elliptical_annulus(self, r0, r1, scale=False):
         if scale:
-            _a0, _b0 = r0*self.a, r0*self.b
-            _a1, _b1 = r1*self.a, r1*self.b
+            a0 = r0*self.a
+            a1, b1 = r1*self.a, r1*self.b
         else:
-            _a0, _b0 = r0, r0*self.b/self.a
-            _a1, _b1 = r1, r1*self.b/self.a
-        return EllipticalAnnulus(self.coords, _a0, _a1, _b1, theta=np.pi/2-self.orientation)
+            a0 = r0,
+            a1, b1 = r1, r1*self.eccentricity
+        return EllipticalAnnulus(self.coords, a0, a1, b1, theta=self.orientation)
 
     def rectangular_annulus(self, r0, r1, scale=False):
         if scale:
-            _a0, _b0 = r0*self.a, r0*self.b
-            _a1, _b1 = r1*self.a, r1*self.b
+            a0 = 2*r0*self.a
+            a1, b1 = 2*r1*self.a, 2*r1*self.b
         else:
-            _a0, _b0 = r0, r0*self.b/self.a
-            _a1, _b1 = r1, r1*self.b/self.a
-        return RectangularAnnulus(self.coords, _a0, _a1, _b1, theta=np.pi/2-self.orientation)
+            a0 = r0
+            a1, b1 = r1, r1*self.eccentricity
+        return RectangularAnnulus(self.coords, a0, a1, b1, theta=self.orientation)
 
     def fit_isophotes(self, debug=False):
         """Fit a photutils.isophote.Ellipse to the source. Requires the source to be instantiated from a skimage RegionProperties
@@ -263,8 +269,7 @@ class Source:
         """
         data = self._region.image_intensity
         y0, x0 = np.unravel_index(np.argmax(data), data.shape)
-        e = self.b / self.a
-        geometry = EllipseGeometry(x0, y0, sma=self.a/4, eps=e, pa=np.pi/2 - self.orientation)
+        geometry = EllipseGeometry(x0, y0, sma=self.a/2, eps=self.eccentricity, pa=self.orientation)
         ellipse = IsoEllipse(data - np.median(data), geometry)
         isolist = ellipse.fit_image()
         
@@ -308,9 +313,9 @@ def auto_source(region, i=None, trace=0.3, extended=0.9, discard=False):
             return DiscardedSource(region, i=i)
         else:
             return PointSource(region, i=i)
-    ratio = b/a
-    if ratio <= extended:
-        if ratio <= trace:
+    eccentricity = b/a
+    if eccentricity <= extended:
+        if eccentricity <= trace:
             return TraceSource(region, i=i)
         else:
             return ExtendedSource(region, i=i)
@@ -386,7 +391,7 @@ class ExtendedSource(Source):
     def _symbol(self):
         return chr(11053)
 
-    def plot(self, radius=6, **kwargs):
+    def plot(self, radius=None, **kwargs):
         """Plot Ellipse on source
 
         Parameters
@@ -436,14 +441,8 @@ class TraceSource(Source):
     def plot(self, offset=10, ax=None, c=color, label=True, fontsize=12):
         if ax is None:
             ax = plt.gca()
-        x0, y0 = self.coords
-        theta = self.orientation
-        x1 = x0 - np.sin(np.pi+theta) * 0.5 * self.a
-        y1 = y0 - np.cos(np.pi+theta) * 0.5 * self.a
-        x2 = x0 - np.sin(theta) * 0.5 * self.a
-        y2 = y0 - np.cos(theta) * 0.5 * self.a
 
-        ax.plot((x1, x2), (y1, y2), c=c)
+        ax.plot(*self.vertexes.T, c=c)
 
         if label and self.i is not None:
             label_coords = self.coords + [0, -offset]
