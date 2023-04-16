bcf32c5990c070043e2020f57d496e061f67b571
lgrcia <lionel_garcia@live.fr>
2023-04-15 17:19:49 -0700

    chore: delete unused

--- prose/blocks/__init__.py
+++ prose/blocks/__init__.py
@@ -8,8 +8,3 @@ from .geometry import *
 from .photometry import *
 from .psf import *
 from .utils import *
-
-
-class DataBlock(Block):
-    def __init__(self, name=None):
-        super().__init__(name)
